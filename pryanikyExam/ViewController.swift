6a2b88c54f75318a572b07e302b972d69f64fff3
kuznetsov <kuznetsov@thehead.ru>
2022-08-23 17:11:49 +0500

    Initial Commit

---
+++ pryanikyExam/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pryanikyExam
+//
+//  Created by Максим Кузнецов on 23.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
